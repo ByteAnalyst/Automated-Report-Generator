6e6123a26dbdf344fce82ac18a2cd9ef8108ed3b
Qurat ul ain <quratulain.sera@gmail.com>
2026-09-02 18:29:53 +0500

    feat: implement CoinGecko market data fetching service

--- BackEnd/Services/Data_Fetcher.py
+++ BackEnd/Services/Data_Fetcher.py
@@ -1,2 +1,26 @@
-def get_API():
-    
+import requests
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_key = os.getenv('api')
+
+def get_coin_data(coin_id: str):
+
+    url = f'https://api.coingecko.com/api/v3/coins/markets'
+
+    parameters = {
+        "vs_currency": 'USD',
+        'ids': coin_id,
+        'x_cg_demo_api_key': API_key
+    }
+
+
+    response = requests.get(url, params = parameters)
+    response.raise_for_status()
+    data = response.json()
+    return data[0] if data else None
+
+print(get_coin_data("bitcoin"))
+
